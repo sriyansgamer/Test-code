9d14eb0cce649df856fa005c658538d4dbf49184
Sriyans Kanneganti <sriyansgamer@gmail.com>
2021-04-28 20:52:45 -0700

    Changed quotations to match my preference.
    I changed the the quotations in the Py file to match my prefernce which is "`".

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,1 @@
-print("Hello world! Just testing if VSC can run Py code in the Integrated Terminal. It looks like it is able to.") #Prints 'Hello world! Just testing if VSC can run Py code in the Integrated Terminal. It looks like it is able to.' in the terminal.
+print('Hello world! Just testing if VSC can run Py code in the Integrated Terminal. It looks like it is able to.') #Prints 'Hello world! Just testing if VSC can run Py code in the Integrated Terminal. It looks like it is able to.' in the terminal.
