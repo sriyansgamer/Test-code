5926e50218a6690b930b63d44de18de8ae90e643
Sriyans Kanneganti <sriyansgamer@gmail.com>
2021-06-18 11:32:14 -0700

    Relinked `.styl` and `.ts` files. I relinked these files with their `.css` and `.js`. I did this by just compilin' them again. Just so I don't get fucked in the future when messin' with 'em.

--- Pie-chart/Pie-chart.js
+++ Pie-chart/Pie-chart.js
@@ -13,7 +13,7 @@ function drawChart() {
     ]);
     // Optional; add a title and set the width and height of the chart
     var options = { 'title': 'My Average Day', 'width': 1000, 'height': 900 }; //'My Avg. Day' title with 1000 px. wide and 900 px. long.
-    // Display the chart inside the <div> element with id="piechart"
+    // Display the chart inside the <div> element with id="piechart."
     var chart = new google.visualization.PieChart(document.getElementById('piechart')); //Displaying the chart under 'piechart' ID.
     chart.draw(data, options); //Drawing 'data' with tasks and hrs. with 'options'.
 }
