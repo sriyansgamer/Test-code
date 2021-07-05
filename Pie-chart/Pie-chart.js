15bcf24da31a603c4254146986ad2f8bead42c77
Sriyans Kanneganti <sriyansgamer@gmail.com>
2021-07-05 14:55:16 -0700

    Oh fuck...

--- Pie-chart/Pie-chart.js
+++ Pie-chart/Pie-chart.js
@@ -1,7 +1,5 @@
-//Load google charts
-google.charts.load('current', { 'packages': ['corechart'] }); //Loadng chart packages.
-google.charts.setOnLoadCallback(drawChart); //Calling finctuon to draw chart on load.
-//Draw the chart and set the chart values
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
         ['Task', 'Hours per Day'],
@@ -9,11 +7,9 @@ function drawChart() {
         ['Eating', 0.5],
         ['Entertainment', 12],
         ['Coding', 0.5],
-        ['Sports', 2] //'Sports' for 2 hr.
+        ['Sports', 2]
     ]);
-    // Optional; add a title and set the width and height of the chart
-    var options = { 'title': 'My Average Day', 'width': 1000, 'height': 900 }; //'My Avg. Day' title with 1000 px. wide and 900 px. long.
-    // Display the chart inside the <div> element with id="piechart."
-    var chart = new google.visualization.PieChart(document.getElementById('piechart')); //Displaying the chart under 'piechart' ID.
-    chart.draw(data, options); //Drawing 'data' with tasks and hrs. with 'options'.
+    var options = { 'title': 'My Average Day', 'width': 1000, 'height': 900 };
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    chart.draw(data, options);
 }
--- Pie-chart/Pie-chart.ts
+++ Pie-chart/Pie-chart.ts
@@ -1,19 +1,15 @@
-//Load google charts
-google.charts.load('current', { 'packages': ['corechart'] }); //Loadng chart packages.
-google.charts.setOnLoadCallback(drawChart); //Calling finctuon to draw chart on load.
-//Draw the chart and set the chart values
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
-        ['Task', 'Hours per Day'], //Tasks done during avg. day. Hrs. done per avg. day.
-        ['Sleeping', 9], //'Sleeping' for 9 hrs.
-        ['Eating', 0.5], //'Eating' for .5 hrs.
-        ['Entertainment', 12], //'Entertainment' for 12 hrs.
-        ['Coding', 0.5], //'Coding' for .5 hrs.
-        ['Sports', 2] //'Sports' for 2 hr.
+        ['Task', 'Hours per Day'],
+        ['Sleeping', 9],
+        ['Eating', 0.5],
+        ['Entertainment', 12],
+        ['Coding', 0.5],
+        ['Sports', 2]
     ]);
-    // Optional; add a title and set the width and height of the chart
-    var options = { 'title': 'My Average Day', 'width': 1000, 'height': 900, }; //'My Avg. Day' title with 1000 px. wide and 900 px. long.
-    // Display the chart inside the <div> element with id="piechart"
-    var chart = new google.visualization.PieChart(document.getElementById('piechart')); //Displaying the chart under 'piechart' ID.
-    chart.draw(data, options); //Drawing 'data' with tasks and hrs. with 'options'.
+    var options = { 'title': 'My Average Day', 'width': 1000, 'height': 900, };
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    chart.draw(data, options);
 }
