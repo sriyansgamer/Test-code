c94d02e3d497b66b42bf123e48b4f20e74800f08
Sriyans Kanneganti <sriyansgamer@gmail.com>
2021-07-11 20:42:48 -0700

    Got js to work with db. Les go, fav lang with mongo noice.

---
+++ MongoDB/mongo.js
@@ -0,0 +1,9 @@
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://sriyansgamer:epicword@datawrite.a5zvh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
